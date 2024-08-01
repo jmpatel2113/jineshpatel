45017402f842074b7f44d7c01c5c08c6dafa8bf7
Jinesh Patel <jmpatel2113@gmail.com>
2024-08-01 14:16:00 -0400

    added google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { Helmet } from 'react-helmet';
 
-import "@fontsource/roboto"
-import "@fontsource/outfit"
+import "@fontsource/roboto";
+import "@fontsource/outfit";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <>
+      <Helmet>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.REACT_PUBLIC_GOOGLE_ANALYTICS_ID}`}></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${import.meta.env.REACT_PUBLIC_GOOGLE_ANALYTICS_ID}');
+          `}
+        </script>
+      </Helmet>
+      <App/>
+    </>
   </React.StrictMode>,
 )

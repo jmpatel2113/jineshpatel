431527f7fde151e2479d0974e32146a57011d421
Jinesh Patel <jmpatel2113@gmail.com>
2024-08-05 22:29:41 -0400

    Ensured safe ga-id

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { Helmet } from 'react-helmet';
 
 import "@fontsource/roboto";
 import "@fontsource/outfit";
 
+function addGtagScript() {
+  const script1 = document.createElement('script');
+  script1.async = true;
+  script1.src = `https://www.googletagmanager.com/gtag/js?id=G-JX82GT3KE7`;
+  document.head.appendChild(script1);
+
+  const script2 = document.createElement('script');
+  script2.innerHTML = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-JX82GT3KE7');
+  `;
+  document.head.appendChild(script2);
+}
+
+document.addEventListener('DOMContentLoaded', addGtagScript);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <>
-      <Helmet>
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${import.meta.env.REACT_PUBLIC_GOOGLE_ANALYTICS_ID}`}></script>
-        <script>
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${import.meta.env.REACT_PUBLIC_GOOGLE_ANALYTICS_ID}');
-          `}
-        </script>
-      </Helmet>
-      <App/>
-    </>
+    <App />
   </React.StrictMode>,
-)
+);

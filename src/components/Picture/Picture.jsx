6c24031b51b3dd5045e9f2cac8a32fcca9543882
jmpatel2113 <jmpatel2113@gmail.com>
2025-08-24 23:01:44 -0400

    added resume button

--- src/components/Picture/Picture.jsx
+++ src/components/Picture/Picture.jsx
@@ -6,18 +6,20 @@ const Picture = () => {
   return (
   <section className={styles.container}>
     <div className={styles.content}>
-        <h1 className={styles.title}>Hi. I'm Jinesh</h1>
-        <p className={styles.description}>I'm a senior at <strong>Rutgers University</strong> majoring in <strong>Computer Science</strong> with a minor in Business Administration. 
-          
-          <p className={styles.description1}>I'm passionate about learning and building <strong>software applications</strong> that address real-world challenges. 
-             With a strong background in <strong>software development</strong> and hands-on project experience, I enjoy creating impactful, user-focused solutions. 
-          </p>
-          <p className={styles.description1}>I'm also an <strong>aspiring cloud application developer</strong> with growing expertise in <strong>AWS</strong> tools and technologies. 
-             Feel free to reach out if you'd like to connect or learn more!
-          </p>
+      <h1 className={styles.title}>Hi. I'm Jinesh</h1>
+      <p className={styles.description}>I'm a senior at <strong>Rutgers University</strong> majoring in <strong>Computer Science</strong> with a minor in Business Administration. 
+        <p className={styles.description1}>I'm passionate about learning and building <strong>software applications</strong> that address real-world challenges. 
+          With a strong background in <strong>software development</strong> and hands-on project experience, I enjoy creating impactful, user-focused solutions. 
         </p>
-        <a href="https://mailto:jmpatel2113@gmail.com" className={styles.contactButton}>Contact Me</a>
-        <img src={getImageUrl("ProfilePicture/ProfilePicture.jpg")} alt="Profile Picture" className={styles.Picture}/>
+        <p className={styles.description1}>I'm also an <strong>aspiring cloud application developer</strong> with growing expertise in <strong>AWS</strong> tools and technologies. 
+          Feel free to reach out if you'd like to connect or learn more!
+        </p>
+      </p>
+       <div className={styles.buttonRow}> 
+        <a href="https://mailto:jmpatel2113@gmail.com" className={styles.contactResumeButton}>Contact Me</a>
+        <a href="/jineshpatel/resume.pdf" className={styles.contactResumeButton} target="_blank" rel="noopener noreferrer">Show Resume</a>
+      </div>  
+      <img src={getImageUrl("ProfilePicture/ProfilePicture.jpg")} alt="Profile Picture" className={styles.Picture}/>
     </div>
   </section>
   );
